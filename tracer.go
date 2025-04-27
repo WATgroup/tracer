c58da2cf07af9a6743c026842a24b2af4e782639
Jose Luis Tallon <jltallon@adv-solutions.net>
2025-04-27 23:37:22 +0200

    Add missing newlines to TRACEx statements

--- tracer.go
+++ tracer.go
@@ -19,6 +19,7 @@ func Tracef(n uint, s string, x ...interface{}) {
 	}
 	fmt.Fprintf(x1, k_TRACE, n)
 	fmt.Fprintf(x1, s, x...)
+	x1.Write([]byte("\n"))
 }
 
 func TraceV(n uint, x ...interface{}) {
@@ -27,15 +28,18 @@ func TraceV(n uint, x ...interface{}) {
 	}
 	fmt.Fprintf(x1, k_TRACE, n)
 	fmt.Fprintln(x1, x...)
+	x1.Write([]byte("\n"))
 }
 
 
 func Debugf(s string, x ...interface{}) {
 	_puts(x2, k_DEBUG)
 	fmt.Fprintf(x2, s, x...)
+	x2.Write([]byte("\n"))
 }
 
 func DebugV(x ...interface{}) {
 	_puts(x1, k_DEBUG)
 	fmt.Fprintln(x2, x...)
+	x1.Write([]byte("\n"))
 }
